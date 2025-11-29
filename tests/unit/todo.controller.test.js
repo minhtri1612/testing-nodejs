42c8cae541164235696fad24383aa1090b13f2bf
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-29 11:23:23 +0700

    Using beforeEach in Jest tests

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -4,16 +4,20 @@ const httpMocks = require('node-mocks-http');
 const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
+let req, res, next;
+
+beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = jest.fn();
+});
 
 describe("TodoController.createTodo", () => {
     it("should create a new todo item", async () => {
         expect(typeof TodoController.createTodo).toBe("function");
     });
     it("should call TodoModel.create with correct parameters", async () => {
-        let req, res, next;
-        req = httpMocks.createRequest();
-        res = httpMocks.createResponse();
-        next = null;
+
         req.body = newTodo;
         TodoController.createTodo(req,res,next);
         // use the correct Jest matcher
